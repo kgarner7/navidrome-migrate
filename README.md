3803a0f732793e051e852eff8b9feb7a5b75272c
Kendall Garner <17521368+kgarner7@users.noreply.github.com>
2023-06-19 16:33:23 -0700

    update scripts, add pre-cimmit

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Usage:
 3. Make note of the original path to your music library.
    This is the `MusicFolder` (navidrome.toml) or `ND_MUSICFOLDER` (environment variable) property
 4. Copy/move your music to the new music folder. (The script will check the validity of the new paths)
-- The new music path may contain symlinks if needed
+   - The new music path may contain symlinks if needed
 5. Run the migration script: `python migrate.py PATH_TO_DB ORIGINAL_MUSIC_FOLDER NEW_MUSIC_FOLDER`.
 6. Change the `MusicFolder`/`ND_MUSICFOLDER` variable to point to the new location of your library.
 7. Start Navidrome, and run a full scan.
--- migrate.py
+++ migrate.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 from argparse import ArgumentParser
 from hashlib import md5
 from pathlib import Path
 from sqlite3 import connect
 from traceback import print_exc
-from typing import NoReturn
+from typing import List, NoReturn, Optional, Tuple
 
 parser = ArgumentParser(description="Navidrome database migrator")
 parser.add_argument("db_path", help="Path to your navidrome.db")
@@ -14,6 +15,8 @@ parser.add_argument(
 parser.add_argument("new_path", help="The current path to your music library")
 
 args = parser.parse_args()
+
+ZERO_WIDTH_SPACE = "\u200b"
 
 
 def fail(msg: str) -> NoReturn:
@@ -30,24 +33,33 @@ if not Path(db_path).is_file():
 
 
 try:
-    with connect(db_path) as conn:
+    conn = connect(db_path, isolation_level=None)
+    try:
         cursor = conn.cursor()
+        cursor.execute("PRAGMA foreign_keys = ON")
+        # Since we are messing with media file ids, this will give us trouble.
+        # defer checking foreign key constraints until the end
+        cursor.execute("PRAGMA defer_foreign_keys = ON")
+        cursor.execute("BEGIN")
 
-        (path_count,) = conn.execute(
+        result: Tuple[int] = conn.execute(
             "SELECT COUNT(*) FROM media_file WHERE path LIKE ? || '%'",
             (old_path,),
         ).fetchone()
+        path_count = result[0]
 
-        (full_count,) = conn.execute("SELECT COUNT(*) from media_file").fetchone()
+        result = conn.execute("SELECT COUNT(*) from media_file").fetchone()
+        full_count = result[0]
 
         if path_count != full_count:
             raise Exception(
                 f"The prefix {old_path} does not match all of the songs in your library ({path_count} vs {full_count}). Please make sure you are using the correct path"
             )
 
-        (sample_path,) = cursor.execute(
+        first_file: Tuple[str] = cursor.execute(
             "SELECT path FROM media_file LIMIT 1"
         ).fetchone()
+        sample_path = first_file[0]
 
         sample_new_path = sample_path.replace(old_path, new_path, 1)
 
@@ -57,19 +69,73 @@ try:
             )
 
         # update media file path and hash
-        cursor.execute("SELECT id, path from media_file")
-        for id, path in cursor.fetchall():
+        media_files: List[Tuple[str, str]] = cursor.execute(
+            "SELECT id, path from media_file"
+        ).fetchall()
+        for id, path in media_files:
             new_track_path = path.replace(old_path, new_path, 1)
             new_id = md5(new_track_path.encode()).hexdigest()
 
             cursor.execute(
                 "UPDATE media_file SET id = ?, path = ? where id = ?",
-                (new_id, new_path, id),
-            )
-            cursor.execute(
-                "UPDATE annotation SET item_id = ? WHERE item_id = ? AND item_type = 'media_file'",
-                (new_id, id),
+                (new_id, new_track_path, id),
             )
 
+            changes = (new_id, id)
+
+            # Update ids of items that reference to this
+            cursor.execute(
+                "UPDATE annotation SET item_id = ? WHERE item_id = ? AND item_type = 'media_file'",
+                changes,
+            )
+
+            cursor.execute(
+                "UPDATE media_file_genres SET media_file_id = ? WHERE media_file_id = ?",
+                changes,
+            )
+
+            cursor.execute(
+                "UPDATE playlist_tracks SET media_file_id = ? WHERE media_file_id = ?",
+                changes,
+            )
+
+            cursor.execute(
+                "UPDATE scrobble_buffer SET media_file_id = ? WHERE media_file_id = ?",
+                changes,
+            )
+
+        # Update smart playlist paths
+        cursor.execute(
+            "UPDATE playlist SET path = ? || SUBSTRING(path, ?) WHERE path != ''",
+            (new_path, len(old_path) + 1),
+        )
+
+        # Update albums
+        albums: List[Tuple[str, str, Optional[str]]] = cursor.execute(
+            "SELECT id, embed_art_path, paths FROM album"
+        ).fetchall()
+        for id, embed, paths in albums:
+            new_embed_path = embed.replace(old_path, new_path, 1)
+
+            if paths:
+                new_paths: Optional[str] = ZERO_WIDTH_SPACE.join(
+                    [
+                        path.replace(old_path, new_path, 1)
+                        for path in paths.split(ZERO_WIDTH_SPACE)
+                    ]
+                )
+            else:
+                new_paths = paths
+
+            cursor.execute(
+                "UPDATE album SET embed_art_path = ?, paths = ? WHERE id = ?",
+                (new_embed_path, new_paths, id),
+            )
+        cursor.execute("COMMIT")
+    except:
+        cursor.execute("ROLLBACK")
+        raise
+
 except Exception as e:
+    print_exc()
     fail(str(e))
